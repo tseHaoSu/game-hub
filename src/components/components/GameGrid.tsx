ac6b0793bbdbd9681dd05413ee0d4c2ddcc6d8ed
tseHaoSu <153491122+tseHaoSu@users.noreply.github.com>
2025-02-25 21:54:59 +1100

    building game cards

--- src/components/components/GameGrid.tsx
+++ src/components/components/GameGrid.tsx
@@ -1,39 +1,17 @@
-import apiClient from "@/services/api-client";
-import { useState, useEffect } from "react";
-import { Text, List } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { List, SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "@/hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => {
-        console.log(res.data.results);
-        setGames(res.data.results)
-      })
-      .catch((err) => setError(err.message));
-  }, []);
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      <List.Root>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} gap={4}>
         {games.map((game) => (
-          <List.Item key={game.id}>{game.name}</List.Item>
+          <GameCard key={game.id} game={game} />
         ))}
-      </List.Root>
+      </SimpleGrid>
     </>
   );
 };
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,38 @@
+import apiClient from "@/services/api-client";
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        console.log(res.data.results);
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
