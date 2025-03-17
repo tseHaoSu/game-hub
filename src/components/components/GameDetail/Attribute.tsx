1807e0f0a578dab9c5997fa7d224595eb056b3ed
tseHaoSu <153491122+tseHaoSu@users.noreply.github.com>
2025-03-17 12:51:36 +1100

    fix npm run build

--- src/components/components/GameDetail/Attribute.tsx
+++ src/components/components/GameDetail/Attribute.tsx
@@ -1,5 +1,5 @@
-import { Heading, VStack, Text } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
+import { Heading, Text, VStack } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface Props {
   term: string;
